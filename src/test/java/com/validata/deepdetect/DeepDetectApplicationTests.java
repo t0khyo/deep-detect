435eb154235d701e637bb00b8bee495d954b2fad
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-12-19 15:58:29 +0200

    First Commit 🚀

---
+++ src/test/java/com/validata/deepdetect/DeepDetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validata.deepdetect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDetectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
