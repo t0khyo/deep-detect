161a3e425d0561f63ca708d4557d1189c04974e2
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-12-26 15:53:25 +0200

    feat: SignatureDetectionController "verify" endpoint

--- src/main/java/com/validata/deepdetect/controller/SignatureDetectionController.java
+++ src/main/java/com/validata/deepdetect/controller/SignatureDetectionController.java
@@ -2,22 +2,35 @@ package com.validata.deepdetect.controller;
 
 import com.validata.deepdetect.dto.SignatureDetectionResponse;
 import com.validata.deepdetect.service.SignatureDetectionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/books")
-@RestController
+@RequestMapping("/api/v1/signature")
 public class SignatureDetectionController {
-    private SignatureDetectionService detectionService;
+    private final SignatureDetectionService detectionService;
 
+    @PostMapping("/verify")
+    @Operation(summary = "Verify signature authenticity", description = "Validates a signature against a genuine reference.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Verification successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<SignatureDetectionResponse> verifySignature(
-            MultipartFile genuineSignature,
-            MultipartFile signatureToVerify
+            @RequestParam MultipartFile genuineSignature,
+            @RequestParam MultipartFile signatureToVerify
     ) {
         return ResponseEntity.ok(detectionService.verifySignature(genuineSignature, signatureToVerify));
     }
+
 }
--- src/main/java/com/validata/deepdetect/service/impl/SignatureDetectionServiceImpl.java
+++ src/main/java/com/validata/deepdetect/service/impl/SignatureDetectionServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.validata.deepdetect.dto.SignatureDetectionResponse;
 import com.validata.deepdetect.service.SignatureDetectionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,23 +14,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SignatureDetectionServiceImpl implements SignatureDetectionService {
-    private final String AI_MODEL_SIGNATURE_ENDPOINT = "https://localhost:8081/signature/predict";
+    private final String AI_MODEL_SIGNATURE_ENDPOINT = "http://localhost:8000/predict";
     private final HttpClient client = HttpClient.newHttpClient();
-    /* todo:
-        this method should call tha ai model api to predict forgery attempts.
-
-        AI_MODEL_SIGNATURE_ENDPOINT accepts these inputs:
-            1. genuineSignature (image file)
-            2. signatureToVerify (image file)
-
-        response should have:
-            1. similarityPercentage (float)
-            2. probabilityPercentage (float)
-            3. signatureWasNotForged (boolean)
-     */
 
     @Override
     public SignatureDetectionResponse verifySignature(MultipartFile genuineSignatureFile, MultipartFile signatureToVerifyFile) {
@@ -38,6 +28,7 @@ public class SignatureDetectionServiceImpl implements SignatureDetectionService 
             String boundary = "Boundary-" + System.currentTimeMillis();
             String body = buildMultipartBody(boundary, genuineSignatureFile, signatureToVerifyFile);
 
+
             // Create the HTTP request
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(AI_MODEL_SIGNATURE_ENDPOINT))
@@ -45,9 +36,13 @@ public class SignatureDetectionServiceImpl implements SignatureDetectionService 
                     .POST(HttpRequest.BodyPublishers.ofString(body))
                     .build();
 
+            log.info("Request: {}", request);
+
             // Send the request and parse the response
             HttpClient client = HttpClient.newHttpClient();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            log.info("Response: {}, body: {}", response, response.body());
 
             // Map JSON response to SignatureDetectionResponse
             ObjectMapper objectMapper = new ObjectMapper();
