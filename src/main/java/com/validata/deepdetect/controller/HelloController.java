0b6548530ecbd15d775511e1b8fbf4ff874eff1d
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2024-12-29 01:56:28 +0200

    added HelloController

---
+++ src/main/java/com/validata/deepdetect/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.validata.deepdetect.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "I hate python :3";
+    }
+}
