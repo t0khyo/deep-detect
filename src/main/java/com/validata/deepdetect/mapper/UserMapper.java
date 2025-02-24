56b9dc98808f31262d3620a4a7420224a1e9b144
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-02-24 09:45:09 +0200

    fix: UserMapper

--- src/main/java/com/validata/deepdetect/mapper/UserMapper.java
+++ src/main/java/com/validata/deepdetect/mapper/UserMapper.java
@@ -4,11 +4,19 @@ import com.validata.deepdetect.dto.SignUpRequest;
 import com.validata.deepdetect.dto.UserResponse;
 import com.validata.deepdetect.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-// todo: not working properly fix it
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "password", target = "password")
     User toEntity(SignUpRequest signUpRequest);
+
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "email", target = "email")
     UserResponse toResponse(User user);
 }
-
