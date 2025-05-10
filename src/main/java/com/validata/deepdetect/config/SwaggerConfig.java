d24975f99bbe091b0cc56f71d7efb7d7b923a89d
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-05-10 03:46:45 +0300

    updated SwaggerConfig

--- src/main/java/com/validata/deepdetect/config/SwaggerConfig.java
+++ src/main/java/com/validata/deepdetect/config/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.validata.deepdetect.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,5 +20,30 @@ public class SwaggerConfig {
                 .group("public")
                 .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI myAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .info(getApiInfo())
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("bearer-jwt", Arrays.asList("read", "write"))
+                                .addList("bearer-key", Collections.emptyList())
+                );
+    }
+
+    private Info getApiInfo() {
+        return new Info()
+                .title("Dar Elsokar API Documentation")
+                .version("1.0.0");
     }
 }
